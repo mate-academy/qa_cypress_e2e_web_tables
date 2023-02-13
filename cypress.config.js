97bac85b47d2aed4890ac701754b978718b027aa
Stepan Prokopyev <stepkapro2002@gmail.com>
2023-02-13 18:37:06 +0200

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  viewportHeight: 1080,
+  viewportWidth: 1320,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'https://demoqa.com/webtables',
   },
 });
--- cypress/e2e/webTables.cy.js
+++ cypress/e2e/webTables.cy.js
@@ -1,5 +1,138 @@
+const { generateUser } = require('../support/generate');
+
 describe('Web Tables page', () => {
-  it('', () => {
-    
+  beforeEach(() => {
+    cy.visit('/');
+  })
+
+  it('should have pagination', () => {
+    cy.findByClassName('-pageInfo')
+      .should('be.visible');
+
+    cy.findByProperty('aria-label', 'jump to page')
+      .should('have.value', '1');
+
+    cy.findByContent('button', 'Previous')
+      .should('be.visible');
+
+    cy.findByContent('button', 'Next')
+      .should('be.visible');
+  });
+
+  it('should have rows-count selector', () => {
+    cy.findByProperty('aria-label', 'rows per page')
+      .should('have.value', '10');
+
+    cy.findByProperty('aria-label', 'rows per page')
+      .select('25');
+
+    cy.findByProperty('aria-label', 'rows per page')
+      .should('have.value', '25');
+  });
+
+  it('should create new worker', () => {
+    const worker = generateUser();
+
+    cy.findByContent('button', 'Add')
+    .should('be.visible');
+
+    cy.findByContent('button', 'Add')
+      .click();
+
+    cy.addNewWorker(worker);
+
+    cy.validateWorker(worker, true);
+
+  });
+
+  it('should remove the worker', () => {
+    const worker = generateUser();
+
+    cy.findByContent('button', 'Add')
+    .should('be.visible');
+
+    cy.findByContent('button', 'Add')
+      .click();
+
+    cy.addNewWorker(worker);
+
+    let len;
+
+    cy.findByProperty('title', 'Delete')
+      .then((elements) => {
+        len = elements.length;
+
+        const rndIdx = Math.round(Math.random() * (len - 1));
+
+        cy.findByProperty('title', 'Delete')
+          .eq(rndIdx)
+          .click({force: true});
+      });
+
+    cy.findByProperty('title', 'Delete')
+      .should('have.length', 3);
+  });
+
+  it('should remove all workers', () => {
+    let len;
+  
+    cy.findByProperty('title', 'Delete')
+      .then((elements) => {
+        len = elements.length;
+        
+        let i = 0;
+
+        while (len > i) {
+          cy.findByProperty('title', 'Delete')
+          .eq(0)
+          .click({force: true});
+
+          i++;
+        }
+      });
+
+    cy.findByContent('div', 'No rows found')
+      .should('be.visible');
+  });
+
+  it.only('should find the worker', () => {
+    const worker = generateUser();
+
+    cy.findByContent('button', 'Add')
+      .click();
+
+    cy.addNewWorker(worker);
+
+    cy.validateByValue(worker.firstName);
+    cy.validateByValue(worker.lastName);
+    cy.validateByValue(worker.email);
+    cy.validateByValue(worker.age);
+    cy.validateByValue(worker.salary);
+    cy.validateByValue(worker.department);
+  });
+
+  it('should edit the worker', () => {
+    let len;
+
+    cy.findByProperty('title', 'Edit')
+      .then((elements) => {
+        len = elements.length;
+
+        const rndIdx = Math.round(Math.random() * (len - 1));
+        
+        cy.findByProperty('title', 'Edit')
+          .eq(rndIdx)
+          .click({force: true});
+
+        const updatedWorker = generateUser();
+
+        cy.addNewWorker(updatedWorker);
+
+        cy.findByProperty('title', 'Edit')
+          .eq(rndIdx)
+          .then(() => {
+            cy.validateWorker(updatedWorker, false);
+          })
+      });
   });
 });
