9cb44281b739391a04b38ca4479b7e5b3914087e
Max Nikiforov <maxxnikiforov@gmail.com>
2023-04-15 17:35:20 +0700

    fist tests added

--- cypress/e2e/webTables.cy.js
+++ cypress/e2e/webTables.cy.js
@@ -1,5 +1,50 @@
+/// <reference types='cypress' />
+import { generateWorker } from "../support/generaiteWorker";
+
 describe('Web Tables page', () => {
-  it('', () => {
-    
+  const worker = generateWorker();
+  beforeEach('', () => {
+    cy.visit('/webtables')
+  });
+
+  it('Page is exist', () => {
+    cy.get('.playgound-header')
+      .should('contain.text', 'Web Tables')
+  });
+
+  it('Pagination is exist', () => {
+    cy.get('.-pagination')
+      .should('exist')
+  });
+
+  it.only('Possibility to Add new worker', () => {
+    cy.get('#addNewRecordButton')
+      .click()
+
+      cy.get('#firstName')
+       .type(worker.firstname)
+
+      cy.get('#lastName')
+        .type(worker.lastname)
+
+      cy.get('#userEmail')
+        .type(worker.email)
+
+      cy.get('#age')
+        .type(worker.age)
+
+      cy.get('#salary')
+        .type(worker.salary)
+
+      cy.get('#department')
+        .type(worker.department)
+
+      cy.get('#submit')
+        .click()
+
+      cy.get('.ReactTable')
+        .should('contain.text', worker.firstname)
+      cy.get('.ReactTable')
+        .should('contain.text', worker.lastname)
   });
 });
