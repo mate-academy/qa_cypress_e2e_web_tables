429b926aa12c7c40f0650f36d7a673030e1e4e36
stanyslav-petryk <petrikstasfb@gmail.com>
2023-06-04 20:24:52 +0400

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require('cypress');
-
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    }
+      viewportHeight: 1080
+      viewportWidth: 1920
+    // implement node event listeners here
+  }
   }
 });
