cafa3ab93aaf56f2023bf8d2f637405a954a4697
Oleksandr_Hundertailo <oleksandrhundertailo@gmail.com>
2023-06-04 22:02:48 +0200

    correct

--- cypress/support/generate.js
+++ cypress/support/generate.js
@@ -1,23 +1,5 @@
 const faker = require('faker');
 
-// function generateUser() {
-//   const firstname = faker.name.firstName();
-//   const lastname = faker.name.lastName();
-//   const email = faker.internet.email();
-//   const age = Math.floor(Math.random(100) * 100);
-//   const salary = Math.floor(Math.random(1000) * 1000);
-//   const department = 'test';
-//   cy.findId('addNewRecordButton').click();
-//   cy.findId('firstName').type(firstname);
-//   cy.findId('lastName').type(lastname);
-//   cy.findId('userEmail').type(email);
-//   cy.findId('age').type(age);
-//   cy.findId('salary').type(salary);
-//   cy.findId('department').type(department);
-//   cy.findId('submit').click();
-// };
-
-// module.exports = { generateUser };
 function generateUser() {
   const firstname = faker.name.firstName();
   const lastname = faker.name.lastName();
