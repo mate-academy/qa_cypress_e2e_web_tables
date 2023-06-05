ec2f03de34b33fae936bd6f42e1005e78cc56871
Artem <protiv55@gmail.com>
2023-06-05 16:42:58 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
+/// <reference types="cypress" />
+
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/webtables',
+    viewportHeight: 1080,
+    viewportWidth: 1960,
     setupNodeEvents(on, config) {
       // implement node event listeners here
-    }
-  }
+    },
+  },
 });
