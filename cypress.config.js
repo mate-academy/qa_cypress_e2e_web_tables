acd453ff68b4c3885a66dbc735afc16e80f56386
Olena Lapa <olena.lapa@gmail.com>
2023-07-20 18:14:47 +0300

    Solution for Web tables

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
 const { defineConfig } = require('cypress');
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/webtables',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        newWorker() {
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            age: Math.random().toString().slice(2, 3),
+            salary: Math.random().toString().slice(2, 3),
+            department: faker.lorem.words()
+          };
+        }
+      });
     }
   }
 });
