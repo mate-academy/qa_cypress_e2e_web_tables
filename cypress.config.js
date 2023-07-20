29640a32c0290d328ff1243d234ce1e78dc12318
Denys Mandebura <mandebura1802@gmail.com>
2023-07-20 17:38:01 +0300

    Revert "hardcorehardcode"
    This reverts commit a9a9025fd05ce7cca6f7295ab8f1b0184a41d31d.

--- cypress.config.js
+++ cypress.config.js
@@ -1,22 +1,9 @@
 const { defineConfig } = require('cypress');
-const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://demoqa.com/webtables',
     setupNodeEvents(on, config) {
-      on('task', {     
-        generateUser() {
-          const email = faker.internet.email();
-            return {
-              firstName: faker.name.firstName(),
-              lastName: faker.name.lastName(),
-              age: Math.floor(Math.random(100) * 100),
-              salary: Math.floor(Math.random(10000) * 10000),
-              email: email.toLowerCase(),
-              department: faker.lorem.word()
-          };
-      }}); 
+      // implement node event listeners here
     }
   }
 });
