6b118810ffe7c40913e6794024e134fef7f033fa
Dmytro Ieromin <dmytroyerominfcdk@gmail.com>
2023-07-20 18:04:25 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,23 @@
 const { defineConfig } = require('cypress');
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/webtables',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        generateWorker() {
+          
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            age: faker.random.number({max: 80}),
+            salary: faker.random.number(),
+            department: 'Insurance'
+          };
+        }
+      });
     }
   }
 });
