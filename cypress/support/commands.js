53d5212c8b822a7b2906d63cd32e7bf72e192b4a
Andrii Chuzhyk <andrey.chuzhik@gmail.com>
2023-07-20 14:17:02 +0300

    solution tables

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,34 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+const faker = require('faker');
+
+Cypress.Commands.add('findById', (id) => {
+  cy.get(`#${id}`);
+});
+
+Cypress.Commands.add('findByPlaceholder', (placeholder) => {
+  cy.get(`[placeholder="${placeholder}"]`);
+});
+
+Cypress.Commands.add('addWorker', () => {
+  cy.findById('addNewRecordButton', 'Add').click();
+  // eslint-disable-next-line max-len
+  cy.findById('registration-form-modal', 'Registration Form').should('be.visible');
+
+  cy.findByPlaceholder('First Name').type(faker.name.firstName());
+
+  cy.findByPlaceholder('Last Name').type(faker.name.lastName());
+
+  cy.findById('userEmail').type(faker.internet.email());
+
+  cy.findByPlaceholder('Age').type(faker.datatype.number({ min: 18, max: 65 }));
+
+  // eslint-disable-next-line max-len
+  cy.findByPlaceholder('Salary').type(faker.random.number({ min: 1, max: 10000 }));
+
+  // eslint-disable-next-line max-len
+  cy.findByPlaceholder('Department').type(faker.random.arrayElement(['Insurance', 'Compliance', 'Legal']));
+
+  cy.findById('submit').click();
+});
