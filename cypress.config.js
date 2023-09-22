83e7f245c3f7b37ac9349f2f03a5f3b827f3af55
Svitlana Zai <svetkaqq1991@gmail.com>
2023-09-22 18:02:04 +0300

    changed config

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,24 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    viewportHeight: 1080,
+    viewportWidth: 1920,
+    baseUrl: 'https://demoqa.com/webtables',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // on('task', {
+      //   generateWorker() {
+      //     const firstName = faker.name.firstName();
+      //     const lastName = faker.name.lastName();
+      //     const email = faker.internet.email();
+      //     const age = faker.random
+      //       .number({ min: 18, max: 60 }).toString();
+      //     const salary = faker.random
+      //       .number({ min: 8000, max: 30000 }).toString();
+      //     const department = faker.commerce.department();
+
+      //     return { firstName, lastName, email, age, salary, department };
+      //   }
+      // })
     }
   }
 });
