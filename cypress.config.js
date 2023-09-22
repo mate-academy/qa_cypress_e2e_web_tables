b64b5689de87d7061d432d50cdef0070566b1c47
Svitlana Zai <svetkaqq1991@gmail.com>
2023-09-22 18:22:25 +0300

    add config

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('cypress');
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
@@ -6,20 +7,20 @@ module.exports = defineConfig({
     viewportWidth: 1920,
     baseUrl: 'https://demoqa.com/webtables',
     setupNodeEvents(on, config) {
-      // on('task', {
-      //   generateWorker() {
-      //     const firstName = faker.name.firstName();
-      //     const lastName = faker.name.lastName();
-      //     const email = faker.internet.email();
-      //     const age = faker.random
-      //       .number({ min: 18, max: 60 }).toString();
-      //     const salary = faker.random
-      //       .number({ min: 8000, max: 30000 }).toString();
-      //     const department = faker.commerce.department();
+      on('task', {
+        generateWorker() {
+          const firstName = faker.name.firstName();
+          const lastName = faker.name.lastName();
+          const email = faker.internet.email();
+          const age = faker.random
+            .number({ min: 18, max: 60 }).toString();
+          const salary = faker.random
+            .number({ min: 8000, max: 30000 }).toString();
+          const department = faker.commerce.department();
 
-      //     return { firstName, lastName, email, age, salary, department };
-      //   }
-      // })
+          return { firstName, lastName, email, age, salary, department };
+        }
+      });
     }
   }
 });
