051cd70cbf15e7592190cab50a686fe0f610aa0f
Vitaliia Dovbak <vitaliyaadovbak+1@gmail.com>
2023-11-08 15:22:08 +0200

    Solution

---
+++ cypress/support/generateWorker.js
@@ -0,0 +1,14 @@
+const faker = require('faker');
+
+function generateWorker() {
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  const email = faker.internet.email();
+  const age = faker.random.number({ min: 18, max: 60 });
+  const salary = faker.random.number({ min: 10000, max: 30000 });
+  const department = faker.commerce.department();
+
+  return { firstName, lastName, email, age, salary, department };
+}
+
+module.exports = { generateWorker };
