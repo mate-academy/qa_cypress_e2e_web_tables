e13d66d126bc7a6d5c3d780dddf804b6f1b96062
Zlata Yermilova <ermzlakl@gmail.com>
2023-11-08 16:51:03 +0200

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/webtables',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+      });
     }
   }
 });
