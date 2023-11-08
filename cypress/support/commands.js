5210e5521cd924173c712c8499bc285642ae8a15
Vladyslav <vlad56445@gmail.com>
2023-11-08 15:46:18 +0200

    Solution

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+const faker = require('faker');
+
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -23,3 +25,51 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('generateRandomWorker', () => {
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+  const email = faker.internet.email();
+  const age = faker.random.number({ min: 18, max: 70 });
+  const department = faker.commerce.department();
+  const salary = faker.random.number({ min: 10000, max: 100000 });
+
+  return {
+    firstName,
+    lastName,
+    email,
+    age,
+    department,
+    salary
+  };
+});
+
+Cypress.Commands.add('AddNewWorker', () => {
+  cy.generateRandomWorker().then((data) => {
+    const {
+      firstName,
+      lastName,
+      email,
+      age,
+      salary,
+      department
+    } = data;
+
+    cy.get('#addNewRecordButton').click();
+
+    cy.get('#firstName').type(firstName);
+    cy.get('#lastName').type(lastName);
+    cy.get('#userEmail').type(email);
+    cy.get('#age').type(age);
+    cy.get('#salary').type(salary);
+    cy.get('#department').type(department);
+    cy.get('#submit').click();
+
+    cy.get('.rt-td').should('contain', firstName);
+    cy.get('.rt-td').should('contain', lastName);
+    cy.get('.rt-td').should('contain', email);
+    cy.get('.rt-td').should('contain', age);
+    cy.get('.rt-td').should('contain', salary);
+    cy.get('.rt-td').should('contain', department);
+  });
+});
