1aea8ccb31035cd163347b193beb4a1a57ad7385
Sasha Kasianenko <kasianenkooleksa@gmail.com>
2023-11-08 17:24:52 +0200

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  projectId: 'hp9zvv',
   e2e: {
     baseUrl: 'https://demoqa.com/',
     setupNodeEvents(on, config) {
