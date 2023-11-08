f885b31923c171a36bfbb234f21871e01ccc422d
KlymSudakov <sudakov.klym@gmail.com>
2023-11-08 18:19:22 +0200

    implement web tables e2e tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,10 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com',
+    viewportHeight: 1500,
+    viewportWidth: 2000,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     }
   }
 });
