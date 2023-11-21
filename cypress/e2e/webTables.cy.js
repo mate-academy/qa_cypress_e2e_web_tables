aa7f63b693d3d7dbf32f7f7abadffa0bfeaf01ea
VladSlob <141657806+VladSlob@users.noreply.github.com>
2023-11-21 08:15:58 +0200

    webTables.cy.js

--- cypress/e2e/webTables.cy.js
+++ cypress/e2e/webTables.cy.js
@@ -1,7 +1,39 @@
 /// <reference types='cypress' />
 
 describe('Web Tables page', () => {
-  it('', () => {
+  beforeEach(() => {
+    cy.visit('https://demoqa.com/webtables');
+  });
 
+  it('checks pagination', () => {
+    
+  });
+
+  it('checks rows count selection', () => {
+    
+  });
+
+  it('adds a new worker', () => {
+    
+  });
+
+  it('deletes a worker', () => {
+    
+  });
+
+  it('deletes all workers', () => {
+    
+  });
+
+  it('finds a worker in the search field and edits it', () => {
+    
+  });
+
+  it('validates data in the worker row after editing the worker', () => {
+    
+  });
+
+  it('checks the search by all column values', () => {
+    
   });
 });
