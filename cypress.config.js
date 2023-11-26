eb18587e4ee794c6e01ce523853f925361985475
Artem Kravets <kravetsartyom@gmail.com>
2023-11-26 19:04:37 +0200

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,24 @@
 const { defineConfig } = require('cypress');
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com',
+    viewportHeight: 1500,
+    viewportWidth: 1500,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        generateUser () {
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            age: faker.random.number({ min: 16, max: 65 }),
+            salary: faker.random.number({ min: 10000, max: 75000 }),
+            department: faker.commerce.department()
+          };
+        }
+      });
     }
   }
 });
