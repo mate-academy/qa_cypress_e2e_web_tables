5d28300b4899635680e766aa7d16fec67801c153
mat12722 <mat1272295@wp.pl>
2023-12-15 12:20:08 +0100

    Solution

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,14 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('addNewWorker', (user) => {
+  cy.get('#addNewRecordButton').click();
+  cy.get('#firstName').type(user.userName);
+  cy.get('#lastName').type(user.userSurname);
+  cy.get('#userEmail').type(user.email);
+  cy.get('#age').type('50');
+  cy.get('#salary').type(user.number);
+  cy.get('#department').type(user.department);
+  cy.get('#submit').click();
+});
