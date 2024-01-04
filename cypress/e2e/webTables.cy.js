5b6cd00f4c04c8f0cac4889808a252e228203091
Orest Halushka <halslor@gmail.com>
2024-01-04 13:38:08 +0200

    tests added

--- cypress/e2e/webTables.cy.js
+++ cypress/e2e/webTables.cy.js
@@ -1,7 +1,113 @@
 /// <reference types='cypress' />
 
 describe('Web Tables page', () => {
-  it('', () => {
+  beforeEach(() => {
+    cy.visit('https://demoqa.com/webtables');
+  });
 
+  const user = {
+    userfirstname: 'Bobby',
+    userlastname: 'Tester',
+    useremail: 'bobby@mail.com',
+    userage: '30',
+    usersalary: '5000',
+    userdepartment: 'QA'
+  };
+
+  it('pagination should be displayed', () => {
+    cy.get('.pagination-bottom')
+      .should('contain.text', 'Page')
+      .and('contain.text', 'of')
+      .and('contain.text', 'Previous')
+      .and('contain.text', 'Next');
+  });
+
+  it('should be able to select the row count', () => {
+    cy.get('[aria-label="rows per page"]').select('5 rows');
+    cy.get('[aria-label="rows per page"]').should('contain', '5 rows');
+    cy.get('[aria-label="rows per page"]').select('10 rows');
+    cy.get('[aria-label="rows per page"]').should('contain', '10 rows');
+    cy.get('[aria-label="rows per page"]').select('20 rows');
+    cy.get('[aria-label="rows per page"]').should('contain', '20 rows');
+  });
+
+  it('should be able to add a new worker', () => {
+    cy.newworker(user);
+    cy.get('.rt-tbody')
+      .should('contain', user.userfirstname)
+      .and('contain', user.userlastname)
+      .and('contain', user.useremail)
+      .and('contain', user.userage)
+      .and('contain', user.usersalary)
+      .and('contain', user.userdepartment);
+  });
+
+  it('should be able to delete a worker', () => {
+    cy.newworker(user);
+    cy.get('#delete-record-4').click();
+    cy.get('.rt-tr-group')
+      .should('not.contain', user.userfirstname)
+      .and('not.contain', user.userlastname)
+      .and('not.contain', user.useremail)
+      .and('not.contain', user.userage)
+      .and('not.contain', user.usersalary)
+      .and('not.contain', user.userdepartment);
+  });
+
+  it('should be able to delete all the workers', () => {
+    cy.get('#delete-record-1').click();
+    cy.get('#delete-record-2').click();
+    cy.get('#delete-record-3').click();
+    cy.get('.rt-tr-group')
+      .should('not.contain', 'Cierra')
+      .and('not.contain', 'Vega')
+      .and('not.contain', 'Alden')
+      .and('not.contain', 'Cantrell')
+      .and('not.contain', 'Kierra')
+      .and('not.contain', 'Gentry');
+  });
+
+  it('should be able to search a worker and edit it', () => {
+    cy.get('#searchBox').type('Cierra');
+    cy.get('.rt-tr-group').should('contain', 'Cierra');
+    cy.get('#edit-record-1').click();
+    cy.get('#firstName').type(`{selectAll}${user.userfirstname}{Enter}`);
+    cy.get('.rt-tr-group').should('contain', user.userfirstname);
+  });
+
+  it('should be able to search worker by First Name', () => {
+    cy.newworker(user);
+    cy.get('#searchBox').type(user.userfirstname);
+    cy.get('.rt-tr-group').should('contain', user.userfirstname);
+  });
+
+  it('should be able to search worker by Last Name', () => {
+    cy.newworker(user);
+    cy.get('#searchBox').type(user.userlastname);
+    cy.get('.rt-tr-group').should('contain', user.userlastname);
+  });
+
+  it('should be able to search worker by Age', () => {
+    cy.newworker(user);
+    cy.get('#searchBox').type(user.userage);
+    cy.get('.rt-tr-group').should('contain', user.userage);
+  });
+
+  it('should be able to search worker by Email', () => {
+    cy.newworker(user);
+    cy.get('#searchBox').type(user.useremail);
+    cy.get('.rt-tr-group').should('contain', user.useremail);
+  });
+
+  it('should be able to search worker by Salary', () => {
+    cy.newworker(user);
+    cy.get('#searchBox').type(user.usersalary);
+    cy.get('.rt-tr-group').should('contain', user.usersalary);
+  });
+
+  it('should be able to search worker by Department', () => {
+    cy.newworker(user);
+    cy.get('#searchBox').type(user.userdepartment);
+    cy.get('.rt-tr-group').should('contain', user.userdepartment);
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,10 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('newworker', (user) => {
+  cy.get('#addNewRecordButton').click();
+  cy.get('#firstName').type(user.userfirstname);
+  cy.get('#lastName').type(user.userlastname);
+  cy.get('#userEmail').type(user.useremail);
+  cy.get('#age').type(user.userage);
+  cy.get('#salary').type(user.usersalary);
+  cy.get('#department').type(user.userdepartment);
+  cy.get('#submit').click();
+});
