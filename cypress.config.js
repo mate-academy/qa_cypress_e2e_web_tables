2b2de6da4d4a7b8bca010d83628ddf60af5d0da8
Yuliia Siromakha <yuliya.o.siromakha@gmail.com>
2024-01-05 02:08:36 +0200

    configs

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,28 @@
 const { defineConfig } = require('cypress');
+const faker = require('faker');
 
 module.exports = defineConfig({
   e2e: {
+    viewportHeight: 1200,
+    viewportWidth:1320,
+    baseUrl: 'https://demoqa.com/webtables',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("task", {
+        newWorker() {
+          randomIndex = Math.floor(Math.random() * 2);         
+
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            age: faker.random.number({ min: 18, max: 65 }),
+            salary: faker.random.number({ min: 2000, max: 100000 }),
+            department: faker.commerce.department(),
+
+       
+      };      
     }
-  }
+   })
+  },
+ },
 });
