ea090686204fa0ec0b716c4cecec11c89295f4b5
Serhii Sharabura <youserhii@gmail.com>
2024-03-19 18:54:20 +0200

    v1_done

--- cypress/e2e/webTables.cy.js
+++ cypress/e2e/webTables.cy.js
@@ -1,7 +1,58 @@
 /// <reference types='cypress' />
-
 describe('Web Tables page', () => {
-  it('', () => {
+  
+  beforeEach(() => {
+    cy.visit('/webtables');
+  });
+   
 
+  it('should check pagination', () => {
+    cy.generate_N_Users(10);
+    cy.contains('.-btn', 'Next').click();
+    cy.get('[aria-label="jump to page"]').should('have.value',
+     '2', {timeout: 10000});
+  });
+
+  it('should check rows count selection', () => {
+    cy.get('[aria-label="rows per page"]').select('5');
+    cy.get('[aria-label="rows per page"]').should('contain', '5');
+  });
+
+  it('should check adding a new worker', () => {
+    cy.generate_N_Users(1);
+    cy.get(':nth-child(4)').should('not.have.value', null);
+  });
+
+  it('should check deletion of a worker', () => {
+    cy.generate_N_Users(1);
+    cy.get('#delete-record-4').click();
+    cy.get(':nth-child(4)').should('have.value', '');
+  });
+
+  it('should check deletion of all workers', () => {
+    cy.get('#delete-record-3').click();
+    cy.get('#delete-record-2').click();
+    cy.get('#delete-record-1').click();
+    cy.get(':nth-child(1)').should('have.value', '');
+  });
+
+  it('should check searching the worker and edition of his data', () => {
+    cy.get('#searchBox').type('Alden{enter}');
+    cy.get(':nth-child(1)').should('contain', 'Alden');
+    cy.get('#edit-record-2').click();
+    cy.get('#firstName').clear().type('Tester');
+    cy.get('#submit').click();
+    cy.get(':nth-child(1)').should('contain', 'Tester');
+  });
+
+  it('should check Email validation after editing the worker', () => {
+    cy.get('#edit-record-1').click();
+    cy.get('input:invalid').should('have.length', 0)
+    cy.get('#userEmail').clear().type('!@#$%^{enter}');
+    cy.get('input:invalid').should('have.length', 1)
+  });
+
+  it.only('should check the search by all column values', () => {
+    cy.searchByColVal('Age', '29');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,51 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('generate_N_Users', (n) => {
+  let user;
+  for (let i = 0; i < n; i++) {
+    cy.task('generateUser').then(generateUser => {
+      user = generateUser;
+      cy.get('#addNewRecordButton').click();
+      cy.get('#firstName').type(user.firstName);
+      cy.get('#lastName').type(user.lastName);
+      cy.get('#userEmail').type(user.email);
+      cy.get('#age').type(user.age);
+      cy.get('#salary').type(user.salary);
+      cy.get('#department').type(user.department);
+      cy.get('#submit').click();  
+    });     
+  };
+});
+
+Cypress.Commands.add('searchByColVal', (columnName, value) => {
+  // const columnName = columnName;
+  // const value = value;
+  let n;
+  if (columnName = 'First Name') {
+    n = 1
+  } 
+
+  if (columnName = 'Last Name') {
+    n = 2
+  }   
+
+  if (columnName = 'Age') {
+    n = 3
+  }   
+
+  if (columnName = 'Email') {
+    n = 4
+  }   
+  
+  if (columnName = 'Salary') {
+    n = 5
+  } 
+  
+  if (columnName = 'Department') {
+    n = 6
+  } 
+     
+  cy.get('#searchBox').type(value, '{enter}');
+  cy.get(':nth-child(n)').should('contain', value);
+});
