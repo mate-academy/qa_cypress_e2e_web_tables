c604cae83a2d7f766cc7782f7580f68b2c0fb19b
Dmytro <dped1986@gmail.com>
2024-03-19 17:36:19 +0200

    Solution

--- cypress/e2e/webTables.cy.js
+++ cypress/e2e/webTables.cy.js
@@ -1,7 +1,107 @@
+/* eslint-disable max-len */
 /// <reference types='cypress' />
+const { faker } = require("@faker-js/faker");
 
 describe('Web Tables page', () => {
-  it('', () => {
+  const user = {
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+    age: faker.number.int({ min: 13, max: 70 }),
+    salary: faker.number.int({ min: 2000, max: 10000 })
+  };
+  beforeEach(() => {
+    cy.visit('/webtables');
+  });
+  it('should provide an opportunity to switch a page', () => {
+    cy.addManyUsers(3);
+    cy.get('select').select('5 rows');
+    cy.get('.-next').click();
 
+    cy.get('[aria-label = "jump to page"]').should('contain.value', 2);
+  });
+
+  it('should provide an opportunity to switch rows count selection', () => {
+    cy.get('select').select('5 rows');
+
+    cy.get('[aria-label="rows per page"]').should('contain.text', '5 rows');
+  });
+
+  it('should provide an opportunity to add a user', () => {
+    cy.get('#addNewRecordButton').click();
+    cy.get('#firstName').type(user.firstName);
+    cy.get('#lastName').type(user.lastName);
+    cy.get('#userEmail').type(user.email);
+    cy.get('#age').type(user.age);
+    cy.get('#salary').type(user.salary);
+    cy.get('#department').type('QA');
+    cy.get('#submit').click();
+
+    cy.get('.rt-td').should('contain', user.firstName);
+  });
+
+  it('should provide an opportunity to delete a worker', () => {
+    cy.get('#addNewRecordButton').click();
+    cy.get('#firstName').type(user.firstName);
+    cy.get('#lastName').type(user.lastName);
+    cy.get('#userEmail').type(user.email);
+    cy.get('#age').type(user.age);
+    cy.get('#salary').type(user.salary);
+    cy.get('#department').type('QA');
+    cy.get('#submit').click();
+    cy.get('#delete-record-4').click();
+
+    cy.get('.rt-td').should('not.contain', user.firstName);
+  });
+
+  it('should provide an opportunity to delete all workers', () => {
+    cy.get('#delete-record-1').click();
+    cy.get('#delete-record-2').click();
+    cy.get('#delete-record-3').click();
+
+    cy.get('.rt-noData').should('contain', 'No rows found');
+  });
+
+  it('should provide an opportunity to edit the worker', () => {
+    cy.get('#searchBox').type('Cierra');
+    cy.get('#edit-record-1').click();
+    cy.get('#department').type('{selectAll} QA');
+    cy.get('#submit').click();
+
+    cy.get('.rt-td').should('contain', 'QA');
+  });
+
+  it.only('should provide an opportunity to find a worker by every column', () => {
+    cy.get('#addNewRecordButton').click();
+    cy.get('#firstName').type(user.firstName);
+    cy.get('#lastName').type(user.lastName);
+    cy.get('#userEmail').type(user.email);
+    cy.get('#age').type(user.age);
+    cy.get('#salary').type(user.salary);
+    cy.get('#department').type('QA');
+    cy.get('#submit').click();
+
+    cy.get('#searchBox').type(user.firstName);
+    cy.get('.rt-td').should('contain', user.firstName);
+
+    cy.get('#searchBox').type('{selectAll}{del}');
+    cy.get('#searchBox').type(user.lastName);
+    cy.get('.rt-td').should('contain', user.lastName);
+
+    cy.get('#searchBox').type('{selectAll}{del}');
+    cy.get('#searchBox').type(user.email);
+    cy.get('.rt-td').should('contain', user.email);
+
+    cy.get('#searchBox').type('{selectAll}{del}');
+    cy.get('#searchBox').type(user.age);
+    cy.get('.rt-td').should('contain', user.age);
+
+    cy.get('#searchBox').type('{selectAll}{del}');
+    cy.get('#searchBox').type(user.salary);
+    cy.get('.rt-td').should('contain', user.salary);
+
+    cy.get('#searchBox').type('{selectAll}{del}');
+    cy.get('#searchBox').type('QA');
+    cy.get('.rt-td').should('contain', 'QA');
   });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 // ***********************************************
 // This example commands.js shows you how to
 // create various custom commands and overwrite
@@ -22,4 +23,31 @@
 //
 //
 // -- This will overwrite an existing command --
+
+const { faker } = require("@faker-js/faker");
+
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('addUser', () => {
+  const user = {
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+    age: faker.number.int({ min: 13, max: 70 }),
+    salary: faker.number.int({ min: 2000, max: 10000 })
+  };
+
+  cy.get('#addNewRecordButton').click();
+  cy.get('#firstName').type(user.firstName);
+  cy.get('#lastName').type(user.lastName);
+  cy.get('#userEmail').type(user.email);
+  cy.get('#age').type(user.age);
+  cy.get('#salary').type(user.salary);
+  cy.get('#department').type('QA');
+  cy.get('#submit').click();
+});
+
+Cypress.Commands.add('addManyUsers', (n) => {
+  for (let i = 0; i < n; i++) {
+    cy.addUser();
+  };
+});
