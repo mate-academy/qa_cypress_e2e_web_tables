a612d41a58a57aa23a32134728f7011199992718
Pavlo Roiev <157385125+SilverF1nger@users.noreply.github.com>
2024-05-16 15:02:39 +0300

    Tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,25 @@
 const { defineConfig } = require('cypress');
 
+const { faker } = require('@faker-js/faker'); // Ensure the correct import for the new faker package
+
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/webtables',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        generateUser() {
+          const user = {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            age: faker.datatype.number({ min: 18, max: 65 }),
+            salary: faker.datatype.number({ min: 30000, max: 100000 }),
+            department: faker.commerce.department(),
+          };
+          return user;
+        }
+      });
+      return config;
     }
   }
 });
