9dcc6951ff2dc179f60b06481bc5a07bdaeb8237
Pavlo Roiev <157385125+SilverF1nger@users.noreply.github.com>
2024-05-17 10:43:46 +0300

    Changes Added

--- cypress/e2e/webTables.cy.js
+++ cypress/e2e/webTables.cy.js
@@ -2,30 +2,14 @@ describe("Web Tables page", () => {
   let user;
 
   beforeEach(() => {
-    cy.task('generateUser').then((generateUser) => {
+    cy.task("generateUser").then((generateUser) => {
       user = generateUser;
     });
     cy.visit("https://demoqa.com/webtables");
   });
 
   it("should have the pagination", () => {
-    cy.get(".-pagination").should("exist");
-    cy.get(".-pagination").should("contain.text", "Previous");
-    cy.get(".-pagination").should("contain.text", "Page");
-    cy.get(".-pagination").should("contain.text", "Next");
-    cy.get(".-pagination .-pageJump input").should("have.attr", "type", "number");
-  });
-
-  it("should have Rows count selection", () => {
-    cy.get('[aria-label="rows per page"]').should("exist");
-    cy.get('[aria-label="rows per page"]').select("5 rows").should('have.value', '5');
-    cy.get('[aria-label="rows per page"]').select("10 rows").should('have.value', '10');
-    cy.get('[aria-label="rows per page"]').select("20 rows").should('have.value', '20');
-    cy.get('[aria-label="rows per page"]').select("25 rows").should('have.value', '25');
-  });
-
-  it("should add a new worker", () => {
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 4; i++) {
       cy.log(`Iteration ${i + 1}`);
       cy.get("#addNewRecordButton").click();
       cy.get("#firstName").type(user.firstName);
@@ -35,16 +19,36 @@ describe("Web Tables page", () => {
       cy.get("#salary").type(user.salary);
       cy.get("#department").type(user.department);
       cy.get("#submit").click();
-      cy.get(".rt-tbody").should("contain", user.firstName);
-      cy.get(".rt-tbody").should("contain", user.lastName);
-      cy.get(".rt-tbody").should("contain", user.email);
-      cy.get(".rt-tbody").should("contain", user.age);
-      cy.get(".rt-tbody").should("contain", user.salary);
-      cy.get(".rt-tbody").should("contain", user.department);
     }
+    cy.get('[aria-label="rows per page"]').select("5 rows");
+    cy.get(".-pagination").should("exist");
+    cy.get(".-pagination").should("contain.text", "Next").click();
+    cy.get(".-pagination").should("contain.text", "Previous").click();
+    cy.get(".-pagination").should("contain.text", "Page");
+    cy.get(".-pagination .-pageJump input").should(
+      "have.attr",
+      "type",
+      "number"
+    );
   });
 
-  it("should validate data in worker row after creating worker", () => {
+  it("should have Rows count selection", () => {
+    cy.get('[aria-label="rows per page"]').should("exist");
+    cy.get('[aria-label="rows per page"]')
+      .select("5 rows")
+      .should("have.value", "5");
+    cy.get('[aria-label="rows per page"]')
+      .select("10 rows")
+      .should("have.value", "10");
+    cy.get('[aria-label="rows per page"]')
+      .select("20 rows")
+      .should("have.value", "20");
+    cy.get('[aria-label="rows per page"]')
+      .select("25 rows")
+      .should("have.value", "25");
+  });
+
+  it("should add a new worker and validate data in worker row after creating", () => {
     cy.get("#addNewRecordButton").click();
     cy.get("#firstName").type(user.firstName);
     cy.get("#lastName").type(user.lastName);
@@ -53,13 +57,12 @@ describe("Web Tables page", () => {
     cy.get("#salary").type(user.salary);
     cy.get("#department").type(user.department);
     cy.get("#submit").click();
-
-    cy.get(".rt-td").should("contain", user.firstName);
-    cy.get(".rt-td").should("contain", user.lastName);
-    cy.get(".rt-td").should("contain", user.email);
-    cy.get(".rt-td").should("contain", user.age);
-    cy.get(".rt-td").should("contain", user.salary);
-    cy.get(".rt-td").should("contain", user.department);
+    cy.get(".rt-tbody").should("contain", user.firstName);
+    cy.get(".rt-tbody").should("contain", user.lastName);
+    cy.get(".rt-tbody").should("contain", user.email);
+    cy.get(".rt-tbody").should("contain", user.age);
+    cy.get(".rt-tbody").should("contain", user.salary);
+    cy.get(".rt-tbody").should("contain", user.department);
   });
 
   it("should find worker in search field and edit it", () => {
@@ -74,9 +77,11 @@ describe("Web Tables page", () => {
 
     cy.get("#searchBox").type(user.firstName);
     cy.get(".rt-td").should("contain", user.firstName);
-    cy.get('.rt-table').scrollIntoView().scrollTo('right');
+    cy.get(".rt-table").scrollIntoView().scrollTo("right");
     cy.get("#edit-record-4 > svg").click();
-    cy.get("#firstName").clear().type(user.firstName + user.lastName);
+    cy.get("#firstName")
+      .clear()
+      .type(user.firstName + user.lastName);
     cy.get("#lastName").clear().type(user.lastName);
     cy.get("#userEmail").clear().type(user.email);
     cy.get("#age").clear().type(user.age);
@@ -126,34 +131,18 @@ describe("Web Tables page", () => {
     cy.get("#department").type(user.department);
     cy.get("#submit").click();
 
-    cy.get("#delete-record-1").click();
+    cy.get(".rt-table").scrollIntoView().scrollTo("right");
+
+    cy.get("#delete-record-4 > svg").click();
     cy.get(".rt-tbody").should("not.contain", user.firstName);
     cy.get(".rt-tbody").should("not.contain", user.lastName);
     cy.get(".rt-tbody").should("not.contain", user.email);
   });
 
-  it("should delete workers by Delete buttons", () => {
-    cy.get("#addNewRecordButton").click();
-    cy.get("#firstName").type(user.firstName);
-    cy.get("#lastName").type(user.lastName);
-    cy.get("#userEmail").type(user.email);
-    cy.get("#age").type(user.age);
-    cy.get("#salary").type(user.salary);
-    cy.get("#department").type(user.department);
-    cy.get("#submit").click();
-
-    cy.get("#addNewRecordButton").click();
-    cy.get("#firstName").type(user.firstName);
-    cy.get("#lastName").type(user.lastName);
-    cy.get("#userEmail").type(user.email);
-    cy.get("#age").type(user.age);
-    cy.get("#salary").type(user.salary);
-    cy.get("#department").type(user.department);
-    cy.get("#submit").click();
-
-    cy.get("#delete-record-1").click();
-    cy.get(".rt-tbody").should("not.contain", user.firstName);
-    cy.get("#delete-record-2").click();
-    cy.get(".rt-tbody").should("not.contain", user.firstName);
+  it.only("should delete workers by Delete buttons", () => {
+    cy.get("#delete-record-1 > svg").click();
+    cy.get("#delete-record-2 > svg").click();
+    cy.get("#delete-record-3 > svg").click();
+    cy.get(".rt-noData").should("contain.text", "No rows found");
   });
 });
