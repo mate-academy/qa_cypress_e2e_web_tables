fec935096e62673258266a5667030a2897cd1212
Voronaolga <voronaolga971@gmail.com>
2024-07-29 13:09:35 +0300

    added support.js

--- cypress/e2e/webTables.cy.js
+++ cypress/e2e/webTables.cy.js
@@ -1,23 +1,5 @@
 /// <reference types='cypress' />
-
-// Define the createUser function
-const createUser = (firstName, lastName, email, age, salary, department) => {
-  // Click on the add button
-  cy.get('#addNewRecordButton').click();
-  // Fill in the new worker details
-  cy.get('#firstName').type(firstName);
-  cy.get('#lastName').type(lastName);
-  cy.get('#userEmail').type(email);
-  cy.get('#age').type(age);
-  cy.get('#salary').type(salary);
-  cy.get('#department').type(department);
-
-  // Click the submit button
-  cy.get('#submit').click();
-
-  // Return user details as an object
-  return { firstName, lastName, email, age, salary, department };
-};
+import { createUser } from './support.js';
 
 describe('Web Tables Pagination', () => {
   it('Should allow to confirm that pagination buttons are present', () => {
