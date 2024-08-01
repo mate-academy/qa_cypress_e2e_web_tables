3f9ecb4fecac0aa2d04d143e423d3303d4c856f9
Maryna <marish1235@gmail.com>
2024-08-01 16:49:31 +0300

    Solution

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
 const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/webtables',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-    }
-  }
+      on('task', {
+        generateUser() {
+          return {
+            firstName: faker.name.firstName(),
+            lastName: faker.name.lastName(),
+            email: faker.internet.email(),
+            salary: '8000',
+            age: '40',
+            department: 'IT'
+          };
+        },
+      });
+    },
+  },
 });
--- cypress/e2e/webTables.cy.js
+++ cypress/e2e/webTables.cy.js
@@ -1,7 +1,80 @@
 /// <reference types='cypress' />
 
 describe('Web Tables page', () => {
-  it('', () => {
+  let user;
 
+  beforeEach(() =>{
+    cy.visit('/');
+    cy.task('generateUser').then(generateUser => {
+      user = generateUser;
+    });
+  });
+
+  it('The pagination and rows count selection', () => {
+    cy.get('[value="5"]').should('exist');
+    cy.get('[aria-label="rows per page"]').should('exist');
+    cy.get('[aria-label="rows per page"]').select('25 rows');
+    cy.get('.rt-tr-group').should('have.length', 25);
+  });
+
+  it('Added a new worker and delete him', () => {
+    cy.get('#addNewRecordButton').click();
+    cy.findByPlaceholder('First Name').type(user.firstName);
+    cy.findByPlaceholder('Last Name').type(user.lastName);
+    cy.findByPlaceholder('name@example.com').type(user.email);
+    cy.findByPlaceholder('Age').type(user.age);
+    cy.findByPlaceholder('Salary').type(user.salary);
+    cy.findByPlaceholder('Department').type(user.department);
+    cy.get('#submit').click();
+    cy.get('#delete-record-4').click();
+    cy.get('.rt-tbody').should('not.contain', user.firstName).and('not.contain', user.lastName);
+  });
+
+  it('Deleteed all workers', () => {
+    cy.get('#delete-record-1').click();
+    cy.get('#delete-record-2').click();
+    cy.get('#delete-record-3').click();
+    cy.get('.rt-noData').should('be.visible');
+  });
+
+  it('Find worker in the search field and edit it', () => {
+    cy.get('#searchBox').type('Alden');
+    cy.get('#edit-record-2').click();
+    cy.findByPlaceholder('Last Name').clear();
+    cy.findByPlaceholder('Last Name').type('Ivanov');
+    cy.get('#submit').click();
+    cy.get('.rt-td').should('contain', 'Ivanov');
+  });
+
+  it.only('Search by all column values', () => {
+    cy.findByPlaceholder('Type to search').type('Kierra');
+    cy.get('.rt-td').should('contain', 'Kierra');
+    cy.get('.rt-td').should('not.contain', 'Alden');
+    cy.findByPlaceholder('Type to search').clear();
+
+    cy.findByPlaceholder('Type to search').type('Cantrell');
+    cy.get('.rt-td').should('contain', 'Cantrell');
+    cy.get('.rt-td').should('not.contain', 'Gentry');
+    cy.findByPlaceholder('Type to search').clear();
+
+    cy.findByPlaceholder('Type to search').type('45');
+    cy.get('.rt-td').should('contain', '45');
+    cy.get('.rt-td').should('not.contain', '29');
+    cy.findByPlaceholder('Type to search').clear();
+
+    cy.findByPlaceholder('Type to search').type('cierra@example.com');
+    cy.get('.rt-td').should('contain', 'cierra@example.com');
+    cy.get('.rt-td').should('not.contain', 'alden@example.com');
+    cy.findByPlaceholder('Type to search').clear();
+
+    cy.findByPlaceholder('Type to search').type('10000');
+    cy.get('.rt-td').should('contain', '10000');
+    cy.get('.rt-td').should('not.contain', '2000');
+    cy.findByPlaceholder('Type to search').clear();
+
+    cy.findByPlaceholder('Type to search').type('Compliance');
+    cy.get('.rt-td').should('contain', 'Compliance');
+    cy.get('.rt-td').should('not.contain', 'Legal');
+    cy.findByPlaceholder('Type to search').clear();
   });
 });
