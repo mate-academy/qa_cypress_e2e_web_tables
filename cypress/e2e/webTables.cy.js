20b8b70ea18d4e9358416414f5883b4b24d09a8a
Olexander Bielov <alehandro.bel96@gmail.com>
2024-08-28 23:38:11 +0300

    test data are ready

--- cypress/e2e/webTables.cy.js
+++ cypress/e2e/webTables.cy.js
@@ -1,7 +1,38 @@
 /// <reference types='cypress' />
 
 describe('Web Tables page', () => {
-  it('', () => {
+  beforeEach(() => {
+    cy.wrap(generateWorker()).as('worker');
 
+    cy.visit();
+  });
+
+  it('should allow to add a new worker', () => {
+    cy.get('@worker').then((worker) => {
+      const {
+        firstName,
+        lastName,
+        email,
+        age,
+        salary,
+        department
+      } = worker;
+
+      cy.getById('firstName')
+        .type(firstName);
+      cy.getById('lastName')
+        .type(lastName);
+      cy.getById('email')
+        .type(email);
+      cy.getById('age')
+        .type(age);
+      cy.getById('salary')
+        .type(salary);
+      cy.getById('department')
+        .type(department);
+    });
+
+    cy.get('button[id=submit]')
+      .click();
   });
 });
