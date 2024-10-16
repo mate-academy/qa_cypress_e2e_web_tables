886daa2d528478be891f49a61af4ba91ba85429a
nevermore <gritsanowo@gmail.com>
2024-10-16 18:41:15 +0300

    solution

--- cypress/support/generate.js
+++ cypress/support/generate.js
@@ -1,13 +1,12 @@
 import { faker } from '@faker-js/faker';
 
 function generateWorker() {
-  const username = faker.internet.userName();
   const firstName = faker.name.firstName();
   const lastName = faker.name.lastName();
-  const email = `${username}@mail.com`;
+  const email = faker.internet.email();
   const age = faker.number.bigInt({ max: 60 });
   const salary = faker.number.bigInt({ min: 300, max: 6000 });
-  const department = faker.word.noun();
+  const department = faker.lorem.word();
 
   return { email, firstName, lastName, age, salary, department };
 }
