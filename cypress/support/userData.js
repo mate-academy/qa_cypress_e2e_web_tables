39c4aa9fea8e71ef563227e577213720f31a2cd1
Sofi Mamzer <sofiiamamzer06@gmail.com>
2024-10-18 18:41:44 +0300

    Web Tables

---
+++ cypress/support/userData.js
@@ -0,0 +1,11 @@
+const { faker } = require('@faker-js/faker');
+
+export const workerData = () => {
+  return {
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+    salary: faker.number.int({ min: 30000, max: 100000 }),
+    department: faker.commerce.department()
+  };
+};
