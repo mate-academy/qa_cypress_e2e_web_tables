26161d8125431fe664e138cda42fc9c24fae713a
Galers <galosaandrew@gmail.com>
2025-01-25 16:04:36 +0200

    test: Add tests

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,22 @@
 const { defineConfig } = require('cypress');
+const { faker } = require('@faker-js/faker');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://demoqa.com/webtables',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('task', {
+        generateUser() {
+          return {
+            name: faker.person.firstName(),
+            lastName: faker.person.lastName(),
+            email: faker.internet.email(),
+            age: faker.number.int({ max: 100 }),
+            salary: faker.number.int({ min: 600, max: 12000 }),
+            department: faker.person.jobArea()
+          };
+        }
+      });
     }
   }
 });
